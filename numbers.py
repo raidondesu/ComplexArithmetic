0b1c4d8482adc1d2b2c1cde54a2997698581bc43
ray <raidonmgmt@gmail.com>
2020-06-03 15:41:57 -0400

    Powers of i
    Imported testing.py (backend of assignment)
    Given an even integer returned the nth power of i else

--- numbers.py
+++ numbers.py
@@ -10,3 +10,14 @@ Polar = Tuple[float, float]
 # x^2 = -1 (X has no solution among real numbers)
 # i^2 = -1 (So instead we say there exists some number that solves that equation) (An imaginary unit)
 # i + i = 2i, i - i = 0, (-1)(i) = -i, (-i)^2 = -1
+#
+# Powers of i
+# Input : an even integer n
+# Goal : Return the nth power of i, or i^n
+@exercise
+def imaginary_power(n : int) -> int:
+    # If n is divisible by 4
+    if n % 4 == 0:
+        return 1
+    else:
+        return -1 
