4207153284a1a54030859122d51ea306715afffb
ray <raidonmgmt@gmail.com>
2020-06-03 12:06:40 -0400

    Imaginary Numbers

--- numbers.py
+++ numbers.py
@@ -2,3 +2,11 @@ from testing import exercise
 from typing import Tuple
 
 import math
+
+Complex = Tuple[float, float]
+Polar = Tuple[float, float]
+
+# Imaginary Numbers
+# x^2 = -1 (X has no solution among real numbers)
+# i^2 = -1 (So instead we say there exists some number that solves that equation) (An imaginary unit)
+# i + i = 2i, i - i = 0, (-1)(i) = -i, (-i)^2 = -1
